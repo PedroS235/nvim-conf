8639a28089b38fcb5b70151384dc807094a4d4e5
Pedro Soares <pmbs.123@gmail.com>
2023-04-26 09:31:40 +0200

    [style] Applied formatting

--- lua/pedros/lsp/null-ls.lua
+++ lua/pedros/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-	print("error when loading null-ls plugin")
-	return
+    print("error when loading null-ls plugin")
+    return
 end
 
 local formatting = null_ls.builtins.formatting
@@ -11,44 +11,45 @@ local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	sources = {
-		-- python
-		formatting.black.with({ extra_args = { "-l", "79" } }),
+    sources = {
+        -- python
+        formatting.black.with({ extra_args = { "-l", "79" } }),
 
-		-- formatting.autopep8,
-		diagnostics.flake8,
+        -- formatting.autopep8,
+        diagnostics.flake8,
 
-		-- lua
-		formatting.stylua,
+        -- lua
+        formatting.stylua,
 
-		-- shell
-		formatting.beautysh,
+        -- shell
+        formatting.beautysh,
 
-		diagnostics.shellcheck,
+        diagnostics.shellcheck,
 
-		-- C/C++
-		formatting.clang_format,
+        -- C/C++
+        formatting.clang_format,
+        --diagnostics.cpplint,
 
-		-- js/css/html/json/...
-		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
+        -- js/css/html/json/...
+        formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 
-		-- html
-		diagnostics.tidy,
+        -- html
+        diagnostics.tidy,
 
-		formatting.rustfmt,
-		-- formatting.marksman,
-	},
-	-- Autoformatting on save
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+        formatting.rustfmt,
+        -- formatting.marksman,
+    },
+    -- Autoformatting on save
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 })
