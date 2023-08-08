bc4572f33e687c4487c78473d969b99ec7602958
Pedro Soares <pmbs.123@gmail.com>
2023-08-08 16:48:04 +0100

    refactor(plugins): update bufferline config

--- lua/pedros/plugins-cfg/bufferline.lua
+++ lua/pedros/plugins-cfg/bufferline.lua
@@ -1,35 +1,70 @@
-local status, bufferline = pcall(require, 'bufferline')
+local status, bufferline = pcall(require, "bufferline")
 if not status then
-    print("error when loading bufferline plugin")
-    return
+	print("error when loading bufferline plugin")
+	return
 end
 
-
 return bufferline.setup({
-    options = {
-        mode = "buffers", -- set to "tabs" to only show tabpages instead
-        numbers = "both", -- | "ordinal" | "buffer_id" | "both",
-        close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-        -- indicator_icon = "▎", -- Giving an error
-        buffer_close_icon = "",
-        modified_icon = "●",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-        max_name_length = 18,
-        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-        tab_size = 18,
-        diagnostics = "nvim_lsp",
-        diagnostics_update_in_insert = false,
-        separator_style = "thick",
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
+	options = {
+		mode = "buffers", -- set to "tabs" to only show tabpages instead
+		style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+		themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+		numbers = "both",
+		close_command = "bdelete! %d", -- can be a string | function, | false see "Mouse actions"
+		right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+		left_mouse_command = "buffer %d", -- can be a string | function, | false see "Mouse actions"
+		middle_mouse_command = nil, -- can be a string | function, | false see "Mouse actions"
+		indicator = {
+			icon = "▎", -- this should be omitted if indicator style is not 'icon'
+			style = "underline",
+		},
+		buffer_close_icon = "󰅖",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		--- name_formatter can be used to change the buffer's label in the bufferline.
+		--- Please note some names can/will break the
+		--- bufferline so use this at your discretion knowing that it has
+		--- some limitations that will *NOT* be fixed.
+		max_name_length = 18,
+		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+		truncate_names = true, -- whether or not tab names should be truncated
+		tab_size = 18,
+		diagnostics = "nvim_lsp",
+		diagnostics_update_in_insert = false,
 
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            local icon = level:match("error") and " " or " "
-            return " " .. icon .. count
-        end,
-    },
+		-- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			local icon = level:match("error") and " " or " "
+			return " " .. icon .. count
+		end,
+
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				text_align = "center",
+				separator = true,
+			},
+		},
+
+		color_icons = true, -- whether or not to add the filetype icon highlights
+		show_buffer_icons = true, -- disable filetype icons for buffers
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		show_tab_indicators = true,
+		show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+		move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
+		separator_style = "slant",
+		enforce_regular_tabs = false,
+		always_show_bufferline = true,
+
+		hover = {
+			enabled = true,
+			delay = 200,
+			reveal = { "close" },
+		},
+	},
 })
