ab80698aff89d6c974e131ca4f5d99cdf4435afb
Pedro Soares <pmbs.123@gmail.com>
2023-08-29 21:28:39 +0200

    fix(keymap): fix tmux navigation with nvim

--- lua/pedros/core/keymaps.lua
+++ lua/pedros/core/keymaps.lua
@@ -12,19 +12,20 @@ vim.g.mapleader = " "
 
 -- Navigation between active buffers
 keymap("n", "H", ":bprevious<CR>", opts) -- LEFT
-keymap("n", "L", ":bnext<CR>", opts) -- RIGHT
+keymap("n", "L", ":bnext<CR>", opts)     -- RIGHT
 
--- Navigation between windows
-keymap("n", "<C-h>", "<C-w>h", opts) -- LEFT
-keymap("n", "<C-l>", "<C-w>l", opts) -- RIGHT
-keymap("n", "<C-k>", "<C-w>k", opts) -- UP
-keymap("n", "<C-j>", "<C-w>j", opts) -- DOWN
+-- Navigation between windows No need to have this, otherwise there is a
+-- conflict with tmux navigator
+-- keymap("n", "<C-h>", "<C-w>h", opts) -- LEFT
+-- keymap("n", "<C-l>", "<C-w>l", opts) -- RIGHT
+-- keymap("n", "<C-k>", "<C-w>k", opts) -- UP
+-- keymap("n", "<C-j>", "<C-w>j", opts) -- DOWN
 
 -- Resize windows
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts) -- LEFT
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)  -- LEFT
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts) -- RIGHT
-keymap("n", "<C-Up>", ":resize -2<CR>", opts) -- UP
-keymap("n", "<C-Down>", ":resize +2<CR>", opts) -- DOWN
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)             -- UP
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)           -- DOWN
 
 -- Indent line
 keymap("v", "<", "<gv", opts) -- LEFT
@@ -52,9 +53,9 @@ keymap("n", "<leader>c", ":tabnew ~/.config/nvim<CR>")
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
-keymap("n", "<leader>f", require("telescope.builtin").find_files, opts) -- Find Files
-keymap("n", "<leader>lg", require("telescope.builtin").live_grep, opts) -- Live grep
-keymap("n", "<leader>bl", require("telescope.builtin").buffers, opts) -- View current buffers
+keymap("n", "<leader>f", require("telescope.builtin").find_files, opts)   -- Find Files
+keymap("n", "<leader>lg", require("telescope.builtin").live_grep, opts)   -- Live grep
+keymap("n", "<leader>bl", require("telescope.builtin").buffers, opts)     -- View current buffers
 keymap("n", "<leader>dg", require("telescope.builtin").diagnostics, opts) -- View the lsp diagnostics of the current buffers
 
 -- Iluminate
