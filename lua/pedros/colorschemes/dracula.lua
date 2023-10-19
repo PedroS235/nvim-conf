63b1b98fa45cd4d2d5cbd2c4ea9cce5041bac295
Pedro Soares <pmbs.123@gmail.com>
2023-10-19 22:24:25 +0200

    refactor(color): remove transparent background

--- lua/pedros/colorschemes/dracula.lua
+++ lua/pedros/colorschemes/dracula.lua
@@ -35,7 +35,7 @@ dracula.setup({
 	-- show the '~' characters after the end of buffers
 	show_end_of_buffer = true, -- default false
 	-- use transparent background
-	transparent_bg = true, -- default false
+	transparent_bg = false, -- default false
 	-- set custom lualine background color
 	lualine_bg_color = "#44475a", -- default nil
 	-- set italic comment
