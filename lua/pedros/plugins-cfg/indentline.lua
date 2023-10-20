f99e52e9cb60c5086775875e6f3a6bed70be9be8
Pedro Soares <pmbs.123@gmail.com>
2023-10-20 09:38:35 +0200

    feat(plugins): remove the show start/end from indenline

--- lua/pedros/plugins-cfg/indentline.lua
+++ lua/pedros/plugins-cfg/indentline.lua
@@ -21,6 +21,6 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 end)
 
 vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup({ scope = { highlight = highlight } })
+require("ibl").setup({ scope = { highlight = highlight, show_start = false, show_end = false } })
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
