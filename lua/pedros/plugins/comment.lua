da0d26e4793182d27bcd0e89841206ad5ee2b1b8
Pedro Soares <pmbs.123@gmail.com>
2023-12-31 11:14:19 +0100

    chore(plugins): change comment plugin

---
+++ lua/pedros/plugins/comment.lua
@@ -0,0 +1,8 @@
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		-- add any options here
+	},
+	lazy = false,
+}
