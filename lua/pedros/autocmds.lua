aafc9efbece11e5d48e36d3ca119705eaacc8736
Pedro Soares <pmbs.123@gmail.com>
2024-01-07 17:29:29 +0100

    feat(cmds): add useful autocommands

---
+++ lua/pedros/autocmds.lua
@@ -0,0 +1,15 @@
+-- Do not start a new line with a comment, if on a comment line
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
+})
+
+-- Wrap text when editing git commits and markdown files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
