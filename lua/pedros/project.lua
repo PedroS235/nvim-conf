1126a6ef931093ab9581b2754852b7c38fa8803a
Pedro Soares <pmbs.123@gmail.com>
2024-01-07 18:56:57 +0100

    feat: add the project plugin

---
+++ lua/pedros/project.lua
@@ -0,0 +1,24 @@
+local M = {
+	"ahmedkhalf/project.nvim",
+	event = "VeryLazy",
+}
+
+function M.config()
+	vim.keymap.set("n", "<leader>fp", function()
+		require("telescope").extensions.projects.projects()
+	end, { desc = "Find Projects" })
+	require("project_nvim").setup({
+		active = true,
+		on_config_done = nil,
+		manual_mode = false,
+		detection_methods = { "pattern" },
+		patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "pom.xml" },
+		ignore_lsp = {},
+		exclude_dirs = {},
+		show_hidden = false,
+		silent_chdir = true,
+		scope_chdir = "global",
+	})
+end
+
+return M
