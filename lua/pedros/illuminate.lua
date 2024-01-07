fcc0573cdef3caf4bb55763d661eecb1cd2bd7d3
Pedro Soares <pmbs.123@gmail.com>
2024-01-07 14:16:40 +0100

    fix: fix keymap command to go to next/prev reference

--- lua/pedros/illuminate.lua
+++ lua/pedros/illuminate.lua
@@ -1,13 +1,18 @@
 local M = {
 	"RRethy/vim-illuminate",
+	event = "VeryLazy",
 }
 
 function M.config()
-	local opts = { noremap = true, silent = true }
 	local keymap = vim.keymap.set
+	local illuminate = require("illuminate")
 
-	keymap("n", "<C-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', opts)
-	keymap("n", "<C-p>", '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', opts)
+	keymap("n", "<C-n>", function()
+		illuminate.goto_next_reference(true)
+	end, { desc = "Go to next reference in the highlighted group" })
+	keymap("n", "<C-p>", function()
+		illuminate.goto_prev_reference(true)
+	end, { desc = "Go to previous reference in the highlighted group" })
 end
 
 return M
