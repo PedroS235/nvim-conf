c847b53408d590c9af36ce7b31c075dca05c2adf
Pedro Soares <pmbs.123@gmail.com>
2024-03-09 15:13:27 +0100

    feat: clean cmp + remove use of the tab

--- lua/pedros/cmp.lua
+++ lua/pedros/cmp.lua
@@ -52,11 +52,6 @@ function M.config()
 	require("luasnip/loaders/from_vscode").lazy_load()
 	require("luasnip").filetype_extend("typescriptreact", { "html" })
 
-	local check_backspace = function()
-		local col = vim.fn.col(".") - 1
-		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-	end
-
 	local icons = require("pedros.icons")
 
 	cmp.setup({
@@ -68,46 +63,26 @@ function M.config()
 		mapping = cmp.mapping.preset.insert({
 			["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 			["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-			["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-			["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+			["<C-y>"] = cmp.mapping.confirm({ select = true }),
 			["<C-e>"] = cmp.mapping({
 				i = cmp.mapping.abort(),
 				c = cmp.mapping.close(),
 			}),
-			-- Accept currently selected item. If none selected, `select` first item.
-			-- Set `select` to `false` to only confirm explicitly selected items.
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.expandable() then
-					luasnip.expand()
-				elseif luasnip.expand_or_jumpable() then
+			-- <c-l> will move you to the right of each of the expansion locations.
+			-- <c-h> is similar, except moving you backwards.
+			["<C-l>"] = cmp.mapping(function()
+				if luasnip.expand_or_locally_jumpable() then
 					luasnip.expand_or_jump()
-				elseif check_backspace() then
-					fallback()
-				else
-					fallback()
 				end
-			end, {
-				"i",
-				"s",
-			}),
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
+			end, { "i", "s" }),
+			["<C-h>"] = cmp.mapping(function()
+				if luasnip.locally_jumpable(-1) then
 					luasnip.jump(-1)
-				else
-					fallback()
 				end
-			end, {
-				"i",
-				"s",
-			}),
+			end, { "i", "s" }),
 		}),
 		formatting = {
 			fields = { "kind", "abbr", "menu" },
@@ -131,16 +106,6 @@ function M.config()
 					vim_item.kind_hl_group = "CmpItemKindTabnine"
 				end
 
-				if entry.source.name == "crates" then
-					vim_item.kind = icons.misc.Package
-					vim_item.kind_hl_group = "CmpItemKindCrate"
-				end
-
-				if entry.source.name == "lab.quick_data" then
-					vim_item.kind = icons.misc.CircuitBoard
-					vim_item.kind_hl_group = "CmpItemKindConstant"
-				end
-
 				if entry.source.name == "emoji" then
 					vim_item.kind = icons.misc.Smiley
 					vim_item.kind_hl_group = "CmpItemKindEmoji"
@@ -150,7 +115,7 @@ function M.config()
 			end,
 		},
 		sources = {
-			{ name = "copilot" },
+			-- { name = "copilot" },
 			{
 				name = "nvim_lsp",
 				entry_filter = function(entry, ctx)
@@ -175,11 +140,7 @@ function M.config()
 			{ name = "nvim_lua" },
 			{ name = "buffer" },
 			{ name = "path" },
-			{ name = "calc" },
 			{ name = "emoji" },
-			{ name = "treesitter" },
-			{ name = "crates" },
-			{ name = "tmux" },
 		},
 		confirm_opts = {
 			behavior = cmp.ConfirmBehavior.Replace,
@@ -200,7 +161,7 @@ function M.config()
 			},
 		},
 		experimental = {
-			ghost_text = true,
+			ghost_text = false,
 		},
 	})
 end
