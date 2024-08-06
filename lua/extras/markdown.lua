decd166d6443aa47f89e9b5d7be07e0e6520de2d
Pedro Soares <pmbs.123@gmail.com>
2024-08-06 11:06:05 +0200

    chore: update github link

--- lua/extras/markdown.lua
+++ lua/extras/markdown.lua
@@ -1,5 +1,5 @@
 local M = {
-	"MeanderingProgrammer/markdown.nvim",
+	"MeanderingProgrammer/render-markdown.nvim",
 	name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
