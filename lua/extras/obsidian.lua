0da66b4d8dc32bc0551b0d7628470de2dadb8b5d
Pedro Soares <pmbs.123@gmail.com>
2024-10-10 10:35:51 +0200

    feat(obsidia): if custom valt path is non existant, use documents/notes

--- lua/extras/obsidian.lua
+++ lua/extras/obsidian.lua
@@ -11,12 +11,20 @@ local M = {
 
 function M.config()
 	local obsidian = require("obsidian")
+	local backup_path = "~/Documents/Notes/"
+
+	if not vim.fn.isdirectory(backup_path) then
+		vim.fn.mkdir(backup_path, "p")
+	end
+
+	local vault_path = os.getenv("SECOND_BRAIN_VAULT") or backup_path
+
 	obsidian.setup({
 		ui = { enable = false },
 		workspaces = {
 			{
 				name = "Second Brain",
-				path = "~/MEGA/Second_Brain_Vault/",
+				path = vault_path,
 			},
 		},
 
@@ -50,6 +58,21 @@ function M.config()
 				opts = { buffer = true },
 			},
 		},
+
+		---@return table
+		note_frontmatter_func = function(note)
+			local out = { id = note.id, tags = note.tags }
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			return out
+		end,
 	})
 end
 
