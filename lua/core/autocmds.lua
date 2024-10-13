fba8024e589f357eddbda3f2d746171214fba905
Pedro Soares <pmbs.123@gmail.com>
2024-10-13 12:02:33 +0200

    chore: remove wrap on .md files

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -9,6 +9,6 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "gitcommit", "markdown" },
 	callback = function()
-		vim.opt_local.wrap = true
+		-- vim.opt_local.wrap = true
 	end,
 })
