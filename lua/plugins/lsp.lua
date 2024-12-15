1521e4f487939d088ec3981e0a24ba800e2b9aa0
Pedro Soares <pmbs.123@gmail.com>
2024-12-15 17:38:40 +0100

    refactor: remove comments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,16 +3,10 @@ return {
 		"neovim/nvim-lspconfig",
 		enabled = true,
 		dependencies = {
-			-- Automatically install LSPs and related tools to stdpath for Neovim
-			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+			{ "williamboman/mason.nvim", config = true },
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-
-			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
-
-			-- Allows extra capabilities provided by nvim-cmp
 			"hrsh7th/cmp-nvim-lsp",
 		},
 		config = function()
